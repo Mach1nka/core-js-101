4c88ef9ce1ca3940e6781db3e68272b2eaec47c6
Artem <uspenskiyart@gmail.com>
2020-05-12 22:28:45 +0300

    date , regex, obj, promises

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,17 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const data = date.getFullYear();
+  let res;
+  if (data % 4 !== 0) {
+    res = false;
+  } else if (data % 100 !== 0) {
+    res = true;
+  } else if (data % 400 !== 0) {
+    res = false;
+  } else res = true;
+  return res;
 }
 
 
@@ -73,8 +82,18 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const hours = end.getHours() - start.getHours();
+  const minutes = end.getMinutes() - start.getMinutes();
+  const seconds = end.getSeconds() - start.getSeconds();
+  const milliseconds = end.getMilliseconds() - start.getMilliseconds();
+  const res = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10
+    ? `0${minutes}` : minutes}:${seconds < 10
+    ? `0${seconds}` : seconds}`;
+  const millisecond = `.${milliseconds < 10 ? `00${milliseconds}` : milliseconds}`;
+  return res + millisecond;
 }
 
 
